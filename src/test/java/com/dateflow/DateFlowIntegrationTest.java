e931629a9806154af28058744ab94fbd3ef692aa
Adolfo Custidiano <adolfoecs@gmail.com>
2021-12-11 07:54:28 +0000

    adding more integration tests

--- src/test/java/com/dateflow/DateFlowIntegrationTest.java
+++ src/test/java/com/dateflow/DateFlowIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.dateflow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.text.ParseException;
 import java.time.Instant;
@@ -133,5 +135,107 @@ class DateFlowIntegrationTest {
         //Then
         assertThat(result)
                 .isEqualTo("2021-12-09T11:33:43.715Z");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-09T23:33:43.715281Z,10", "2021-12-09T12:33:43.715281Z,9"})
+    void shouldProduceDayUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().day();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-31T23:33:43.715281Z,1", "2021-12-09T12:33:43.715281Z,12"})
+    void shouldProduceMonthUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().month();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-31T23:33:43.715281Z,2022", "2021-12-09T12:33:43.715281Z,2021"})
+    void shouldProduceYearUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().year();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-31T23:33:43.715281Z,8", "2021-12-09T12:33:43.715281Z,21"})
+    void shouldProduceHourUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().hour();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-31T23:33:43.715281Z,33", "2021-12-09T12:33:43.715281Z,33"})
+    void shouldProduceMinUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().minutes();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"2021-12-31T23:33:43.715281Z,43", "2021-12-09T12:33:43.715281Z,43"})
+    void shouldProduceSecondsUsingSpecificTimeZone(String date, int expected) {
+        //Given
+        var instant = Instant.parse(date);
+
+        //When
+        var result = DateFlow.
+                from(instant)
+                .zoneId(ZoneId.of("Asia/Tokyo"))
+                .as().seconds();
+
+        //Then
+        assertThat(result)
+                .isEqualTo(expected);
     }
 }
